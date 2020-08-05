dacf35083d442e8b771adc692e26531e0e0a84de
Aayush Sureshchander <asaayush@gmail.com>
2020-08-05 12:45:03 -0400

    Skeletal Structure of the Code

---
+++ image_SR_1.py
@@ -0,0 +1,40 @@
+# This is a Image Super Resolution Netowrk to be applied eventually on videos.
+
+
+# Import the necessary libraries
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from matplotlib import pyplot as plt
+import cv2
+from keras.preprocessing import image           # For Image Pre-processing
+
+# FUNCTIONS
+# Import the video
+
+
+# Extract the frames
+
+
+# Import Supplementary Images for Increasing Training and Testing Data
+
+
+# Implement Downsizing/Down-sampling Function
+
+
+# Create & Compile Model
+
+
+# Train Model
+
+
+# Test Model Performance
+
+
+# Visualize Performance
+
+
+# Use Low Resolution Videos to Generate High Resolution Videos Using Model
+
+
+# MAIN CODE
