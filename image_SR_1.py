cc3d8fb31bad973e30899e1539403207a849d063
Aayush Sureshchander <asaayush@gmail.com>
2020-08-05 17:57:53 -0400

    Created function to import high resolution images. Next step is to downsize the images ready to feed to the neural network

--- image_SR_1.py
+++ image_SR_1.py
@@ -8,6 +8,9 @@ from tensorflow import keras
 from matplotlib import pyplot as plt
 import cv2
 from keras.preprocessing import image           # For Image Pre-processing
+import PIL
+import pathlib
+import glob
 
 # FUNCTIONS
 # Import the video
@@ -17,6 +20,22 @@ from keras.preprocessing import image           # For Image Pre-processing
 
 
 # Import Supplementary Images for Increasing Training and Testing Data
+def importHighRes(train_dir, batch, img_size, info=0):
+    data_source_training = train_dir
+    # data_source_validation = 'datasets/single_combined/validation'
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(directory=data_source_training,
+                                                                   label_mode=None,
+                                                                   batch_size=batch,
+                                                                   image_size=img_size)
+    # test_ds = tf.keras.preprocessing.image_dataset_from_directory(directory=data_source_validation,
+    #                                                              label_mode=None,
+    #                                                              batch_size=1,
+    #                                                              image_size=(3840, 2160))
+    if info == 1:
+        print(len(train_ds))
+        print(type(train_ds))
+        print(train_ds)
 
 
 # Implement Downsizing/Down-sampling Function
@@ -38,3 +57,7 @@ from keras.preprocessing import image           # For Image Pre-processing
 
 
 # MAIN CODE
+train_directory = 'datasets/single_combined/training'
+batch_size = 16
+image_size = (3840, 2160)
+importHighRes(train_directory, batch_size, image_size, info=1)
